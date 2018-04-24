312329decebf06b4de394e48e0a0da873d048f14
yzha0802 <yzhao0802@gmail.com>
2018-04-25 01:59:30 +0800

    Add files via upload

---
+++ setup-2.py
@@ -0,0 +1,41 @@
+from os.path import dirname, join
+from setuptools import setup, find_packages
+
+
+setup(
+      name='biclustering',
+      version=1.0,
+      url='https://github.com/yzha0802/python-bicluster',
+      description='Implementation of Sparse Singular Value Decomposition',
+      # long_description=open('README').read(),
+      author='Yan Zhao, Rui Wang',
+      #maintainer='Pablo Hoffman',
+      #maintainer_email='pablo@pablohoffman.com',
+      license='BSD',
+      packages=find_packages(exclude=('tests', 'tests.*')),
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={
+      'console_scripts': ['scrapy = scrapy.cmdline:execute']
+      },
+      classifiers=[
+                   'Framework :: biclustering',
+                   'Development Status :: 5 - Production/Stable',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Software Development :: Libraries :: Application Frameworks',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
+      install_requires=[],
+      )
